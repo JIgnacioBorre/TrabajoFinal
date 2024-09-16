1f337ac44e2c74e95a0580b8e4641378adca242a
Juan Cruz Montero <juancruzmontero2312@gmail.com>
2024-09-16 18:44:13 -0300

    Modificacion de main y agregamos clase sala y horarios

--- src/main/java/CineGestor/Sala.java
+++ src/main/java/CineGestor/Sala.java
@@ -1,22 +1,25 @@
 package CineGestor;
 
 public class Sala {
-    private int numero;
-    private int capacidad;
+    private final int numero;
+    private final int capacidad;
 
+    // Constructor para inicializar número y capacidad de la sala
     public Sala(int numero, int capacidad) {
         this.numero = numero;
         this.capacidad = capacidad;
     }
 
-    public int getNumero() {return numero; }
-    public void setNumero(int numero) {this.numero = numero; }
+    public int getNumero() {
+        return numero;
+    }
 
-    public int getCapacidad() {return capacidad; }
-    public void setCapacidad(int capacidad) {this.capacidad = capacidad; }
+    public int getCapacidad() {
+        return capacidad;
+    }
 
+    @Override
     public String toString() {
-        return "Sala" + numero + " - Capacidad: " + capacidad;
+        return "Sala " + numero + " (Capacidad: " + capacidad + ")";
     }
 }
-
